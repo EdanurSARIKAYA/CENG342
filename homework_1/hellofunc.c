5ad2a3d52ffb5cac669b9b6903be09fb0fa70966
EdanurSARIKAYA <100278578+EdanurSARIKAYA@users.noreply.github.com>
2023-04-04 18:01:47 +0300

    New Homework2(with make)

--- homework_1/hellofunc.c
+++ homework_1/hellofunc.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <hellomake.h>
+//Edanur SARIKAYA 19050111004
+    void writeFile_make(char *output_file, double *output, int r) {
+    FILE *file; 
 
-void myPrintHelloMake(void) {
+    file = fopen(output_file, "w");
 
-  printf("Hello makefiles!\n");
 
-  return;
+    if (file == NULL) {
+        printf("CANNOT OPEN.\n");
+        exit(1);
+    }
+
+    int k;
+    fprintf(file, "%s\n", "output");
+    for(k = 0; k<r ;k++){
+    	//Write output to file
+	    fprintf(file, "%.2lf\n", output[k]);
 }
+
+
+    fclose(file);
+    
+     printf("File written successfully\n");
+     return;
+    
+}
+
+   
